ed1390c3696727232187a41537590a004dad01a9
flannois <florent.lannois@gmail.com>
2021-04-26 17:38:39 +0200

    Méthode de calcul

--- calculAire/calcul.py
+++ calculAire/calcul.py
@@ -11,18 +11,21 @@
 from math import acos, pi, sqrt
 
 def calculAngle(a,b,c):
+    # Calcul de l'angle en fonction des a, b et c
     angle = acos((a**2 - b**2 - c**2)/(-2*b*c))
     angle = angle * 180 / pi
     print(angle)
     return angle
 
 def calculDesAngles(a,b,c):
+  # calcul des trois angles (renvoi alpha, beta et phi)
     alpha = calculAngle(a,b,c)
     beta = calculAngle(b,c,a)
     phi = calculAngle(c,a,b)
     return alpha, beta, phi
 
 def rayonCercle(a,b,c):
+    # Calcul du rayon du cercle
     s = (a+b+c)/2
     A = sqrt(s*(s-a)*(s-b)*(s-c))
     rayon = (a*b*c)/(4*A)
