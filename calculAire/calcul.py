7d55821130088508103446c183192d4d81ae46c7
flannois <florent.lannois@gmail.com>
2021-04-27 22:16:36 +0200

    ajout dessin

--- calculAire/calcul.py
+++ calculAire/calcul.py
@@ -4,11 +4,12 @@
                         /     \___  a
                      c /          \___
                       /               \___
-                alpha/_____________________\ phi
+                alpha/____________________\ phi
                                 b
 """
 
 from math import acos, pi, sqrt, atan
+import turtle
 
 def calculAngle(a,b,c):
     # Calcul de l'angle en fonction des a, b et c, a étant la longueur en face de l'angle
@@ -39,6 +40,8 @@ def interieurTriangleHauteur(a, b):
     # Calcul de la hauteur du triangle "intérieur"
     """L'ERREUR EST ICI"""
     # Il faut calculer la hauteur du triangle aligné sur le segment du rayon /!\ Penser aux médiatrices !
+    return 0
+
 
 def exterieurTriangleHauteur(rayonCercle, hauteurTriangle):
     # Calcul de la hauteur du triangle "extérieur"
@@ -61,11 +64,13 @@ def cercleCoupe(aireCercle, angle):
     return aireTotale
 
 
+
 a = 8
-b = 13
-c = 9
+b = 12
+c = 5
 
 alpha, beta, phi = calculDesAngles(a, b, c)
+
 r = rayonCercle(a, b, c)
 aireCercle = aireCercle(r)
 hauteurInterieurTriangle = interieurTriangleHauteur(a, b)
@@ -76,4 +81,29 @@ angle = angleDuBas(b, hauteurExterieurTriangle)
 angle = angleTotalBas(angle)
 
 aireTotale = cercleCoupe(aireCercle, angle)
-print(aireTotale)
+
+alpha, beta, phi = calculDesAngles(a,b,c)
+
+echelle = 50
+vitesse = 10
+
+turtle.speed(vitesse)
+
+turtle.forward(b*echelle)
+turtle.left(180-phi)
+
+turtle.forward(a*echelle)
+turtle.left(180-beta)
+
+turtle.forward(c*echelle)
+turtle.left(180-alpha)
+
+
+turtle.right(beta)
+
+turtle.circle(r*echelle)
+
+
+
+turtle.mainloop()
+
